37f7be781341f75c7e6cf93e6fe605ee74c1cf59
Aryan Singh <aryan.singhfc0605@gmail.com>
2025-05-21 01:16:53 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,12 +1,12 @@
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_community.vectorstores import FAISS
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
 import json
 
 with open('faqs.json', 'r') as file:
     data = json.load(file)
 
 faq_data = []
-
 for section, qa_list in data.items():
     for qa in qa_list:
         faq_data.append({"question": qa["question"], "answer": qa["answer"]})
@@ -14,32 +14,25 @@ for section, qa_list in data.items():
 semantic_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
 
 faq_questions = [faq['question'] for faq in faq_data]
-
-if semantic_model:
-    question_embeddings = FAISS.from_texts(faq_questions, semantic_model)
+question_embeddings = semantic_model.embed_documents(faq_questions)
 
 def get_closest_answer(user_query):
-    docs_with_scores = question_embeddings.similarity_search_with_score(user_query, k=1)
-    
-    question = None
-    threshold = 1.5
-    best_match, score = docs_with_scores[0]
-    content = best_match.page_content
-    
-    if score <= threshold:
-        question = content
+    query_embedding = semantic_model.embed_query(user_query)
 
-    if question is not None:
-        
-        closest_match_idx = faq_questions.index(question)
-        
-        return faq_data[closest_match_idx]['answer'], faq_data[closest_match_idx]['question']
+    similarities = cosine_similarity([query_embedding], question_embeddings)[0] 
+
+    best_idx = np.argmax(similarities)
+    best_score = similarities[best_idx]
+    threshold = 0.75
+
+    if best_score >= threshold:
+        return faq_data[best_idx]['answer'], faq_data[best_idx]['question']
     else:
         return None, None
 
 def chatbot(user_query):
     closest_answer, closest_question = get_closest_answer(user_query)
-    
+
     if closest_answer:
         return closest_answer
     else:
